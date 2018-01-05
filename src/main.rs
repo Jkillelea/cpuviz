aeda0e63f3cbade9dfda194063f6c2f1c182616c
Jacob Killelea <jkillelea@protonmail.ch>
2018-01-04 22:05:22 -0800

    feature flag for generating bindings (otherwise use builtin). Bindings moved to mod gtop::*

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+mod gtop {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(warnings)]
 
-#[cfg(feature = "generate_bindings")]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs")); // use build.rs generated
-#[cfg(not(feature = "generate_bindings"))]
-include!("./bindings.rs"); // use manually generated
+    #[cfg(feature = "generate_bindings")]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs")); // use build.rs generated
 
-use std::mem;
+    #[cfg(not(feature = "generate_bindings"))]
+    include!("./bindings.rs"); // use manually generated
+}
+
+use std::thread;
+use std::time::Duration;
 mod cpu;
+pub use cpu::Cpu;
 
 fn main() {
-    let mut memory: glibtop_mem;
-    unsafe {
-        memory = mem::zeroed();
-        glibtop_init();
-        glibtop_get_mem(&mut memory);
-        glibtop_close();
+
+    let mut cpu = cpu::Cpu::new();
+    for _ in 0 .. 10 {
+        thread::sleep(Duration::from_millis(1000));
+        let (j, _) = cpu.measure();
+        println!("{:?}", j);
     }
-    println!("{:#?}", memory);
 }
