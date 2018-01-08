8607425b2422b98b9e0a48dbb3d969db0129a8a0
Jacob Killelea <jkillelea@protonmail.ch>
2018-01-07 16:16:04 -0800

    misc

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::time::Duration;
 use std::sync::Arc;
 
 mod cpu;
+mod memory;
 mod glibtop_handle;
 mod percent_usage;
 pub use cpu::Cpu;
+pub use memory::Memory;
 pub use glibtop_handle::GLibTopHandle;
 pub use percent_usage::PercentUsage;
 
@@ -25,9 +27,13 @@ mod gtop {
 fn main() {
     let gtop_handle = Arc::new(GLibTopHandle::new()); // Atomic Reference Counted object to call glibtop_init and glibtop_close when we start and when we run out of objects
 
-    let mut cpu = cpu::Cpu::with_handle(gtop_handle.clone());
+    let mut cpu    = Cpu::with_handle(gtop_handle.clone());
+    let mut memory = Memory::with_handle(gtop_handle.clone());
     for _ in 0..100 {
         thread::sleep(Duration::from_secs(1));
-        println!("{:?}", cpu.percent_usage());
+        println!("{}, {}",
+            cpu.percent_usage(),
+            memory.percent_usage(),
+        );
     }
 }
