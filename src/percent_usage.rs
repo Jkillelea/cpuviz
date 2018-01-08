80710aef6311144e1621a7942f8ab44126a149bb
Jacob Killelea <jkillelea@protonmail.ch>
2018-01-07 16:15:51 -0800

    documentation

--- src/percent_usage.rs
+++ src/percent_usage.rs
@@ -1,3 +1,4 @@
 pub trait PercentUsage {
+    /// The percent usage of a resource (eg: the CPU usage). Should return a value between 0.0 (0%) and 1.0 (100%)
     fn percent_usage(&mut self) -> f64; // return a number between 0.0 (0%) and 1.0 (100%)
 }
